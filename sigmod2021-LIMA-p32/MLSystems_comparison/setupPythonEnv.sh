684e6da62b93c716e93993d93bc167a9a9838996
Shadman Saqib Eusuf <S-Saqib@users.noreply.github.com>
2026-05-15 10:35:55 -0400

    Delete other project directories

--- sigmod2021-LIMA-p32/MLSystems_comparison/setupPythonEnv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 -m venv --system-site-packages ./tf-venv
-source tf-venv/bin/activate
-pip install --upgrade pip
-pip install tensorflow==2.3.1
-pip install scikit-learn==0.23.2
-pip install pandas
-deactivate
